e220622d77157bbe195ec33ffd64cd8afb1f4aab
Y7gn <malekkhodary@hotmail.com>
2023-05-28 21:36:52 +0300

    production

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'normalize.css'
-import './index.css';
-import App from './App';
-import { AppProvider } from './context/appContext.js';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "normalize.css";
+import "./index.css";
+import App from "./App";
+import { AppProvider } from "./context/appContext.js";
+// import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+// if (process.env.NODE_ENV === "production") disableReactDevTools();
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div dir='rtl'>
-
-    <AppProvider>
-      <App />
-    </AppProvider>
-
+    <div dir="rtl">
+      <AppProvider>
+        <App />
+      </AppProvider>
     </div>
   </React.StrictMode>
 );
-
-
