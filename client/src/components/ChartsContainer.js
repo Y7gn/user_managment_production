ad38d7d0696cbc3f53e131004a2ad9bfdcc23251
Y7gn <malekkhodary@hotmail.com>
2023-06-01 03:42:00 +0300

    first requirment

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,19 +1,25 @@
-import React, {useState} from 'react'
-import BarChart from './BarChart'
-import AreaChart from './AreaChartComponent'
-import { useAppContext } from '../context/appContext'
-import Wrapper from '../assets/wrappers/ChartsContainer'
+import React, { useState } from "react";
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChartComponent";
+import { useAppContext } from "../context/appContext";
+import Wrapper from "../assets/wrappers/ChartsContainer";
 
 const ChartsContainer = () => {
-    const [barChart, setBarChart ] = useState(true)
-    const { monthlyApplications:data} = useAppContext()
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
   return (
     <Wrapper>
-        <h5>Monthly Applications</h5>
-        <button type='button' onClick={()=> setBarChart(!barChart)}>{barChart? 'AreaChart':'BarChart'}</button>
-        {barChart? <BarChart data={data}/>:<AreaChart data={data}/>}
+      <h5>Monthly Applications</h5>
+      <button
+        type="button"
+        onClick={() => setBarChart(!barChart)}
+        className="statsButton"
+      >
+        {barChart ? "تخطيط مساحي" : "خريطة أعمدة"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
     </Wrapper>
-  )
-}
+  );
+};
 
-export default ChartsContainer
+export default ChartsContainer;
